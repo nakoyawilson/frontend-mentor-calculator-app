c7dc9f146234e962ff81f91f2c3c2aa3dba29cfa
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-02-17 19:00:54 -0400

    Add starter files

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p class="attribution">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://nakoyawilson.netlify.app/">Nakoya Wilson</a>
+        .
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
